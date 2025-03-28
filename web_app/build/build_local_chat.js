6d66e6b4acff80fa094d27577555152653b803e1
Andrew T <andrewtran3643@gmail.com>
2025-03-28 10:51:51 +1030

    added support for building on windows

--- web_app/build/build_local_chat.js
+++ web_app/build/build_local_chat.js
@@ -1,6 +1,7 @@
 import { existsSync, mkdirSync, rmSync, copyFileSync, promises as fs } from "fs";
 import { join } from "path";
 import { execSync } from 'child_process';
+import os from 'os';
 
 
 // Function to recursively copy a directory
@@ -46,17 +47,21 @@ let target_operating_system = args[0];
 let fileserver_exe_name = "";
 let go_build_command = "";
 
-if (target_operating_system === "windows") {
-    fileserver_exe_name = "fileserver.exe"
-    go_build_command = `set GOOS=${target_operating_system} && set GOARCH=amd64 && go build -C ..`;
+// set the go_build_command depending on the current operating system
+if (os.platform().indexOf("win") != -1) {
+    // building on windows
+    go_build_command = `set GOOS=${target_operating_system}&& set GOARCH=amd64&& go build -C ..`;
 
-} else if (target_operating_system === "linux") {
-    fileserver_exe_name = "fileserver"
+} else if (os.platform().indexOf("linux") != -1) {
+    // building on linux
     go_build_command = `GOOS=${target_operating_system} GOARCH=amd64 go build -C ..`;
 
-} else {
-    throw new Error(`${args[0]} is not a supported operating system build target.`);
-}
+} else throw new Error(`Building on unsupported operating system: ${os.platform()}`);
+
+// set the executable name based on the target operating system
+if (target_operating_system === "windows") fileserver_exe_name = "fileserver.exe"
+else if (target_operating_system === "linux") fileserver_exe_name = "fileserver"
+else throw new Error(`${target_operating_system} is not a supported operating system build target.`);
 
 // compile the go fileserver
 let output = execSync(go_build_command, { encoding: "utf8" });
