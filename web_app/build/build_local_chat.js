dab7796343540fb7db05ef4d2e0a8b5e1af49b38
Andrew T <andrewtran3643@gmail.com>
2025-05-06 18:10:06 +0930

    edited build script to install and pkg models

--- web_app/build/build_local_chat.js
+++ web_app/build/build_local_chat.js
@@ -1,43 +1,71 @@
-import { existsSync, mkdirSync, rmSync, copyFileSync, promises as fs } from "fs";
+import { existsSync, mkdirSync, rmSync, copyFileSync, promises as fs, createWriteStream} from "fs";
 import { join } from "path";
 import { execSync } from 'child_process';
 import os from 'os';
+import https from 'follow-redirects/https.js';
+import { dirname } from "path";
 
 
 // Function to recursively copy a directory
 async function copyDirectory(src, dest) {
-    try {
-        // Check if the source directory exists
-        const stats = await fs.stat(src);
-        if (!stats.isDirectory()) {
-            throw new Error(`${src} is not a directory`);
-        }
-
-        // Ensure the destination directory exists
-        await fs.mkdir(dest, { recursive: true });
-
-        // Read all items in the source directory
-        const files = await fs.readdir(src);
-
-        for (const file of files) {
-            const srcPath = join(src, file);
-            const destPath = join(dest, file);
-
-            const fileStats = await fs.stat(srcPath);
-
-            if (fileStats.isDirectory()) {
-                // Recursively copy subdirectories
-                await copyDirectory(srcPath, destPath);
-            } else {
-                // Copy file
-                await fs.copyFile(srcPath, destPath);
-            }
-        }
-
-        console.log(`Directory ${src} copied to ${dest}`);
-    } catch (error) {
-        console.error("Error copying directory:", error);
+  try {
+    // Check if the source directory exists
+    const stats = await fs.stat(src);
+    if (!stats.isDirectory()) {
+      throw new Error(`${src} is not a directory`);
     }
+
+    // Ensure the destination directory exists
+    await fs.mkdir(dest, { recursive: true });
+
+    // Read all items in the source directory
+    const files = await fs.readdir(src);
+
+    for (const file of files) {
+      const srcPath = join(src, file);
+      const destPath = join(dest, file);
+
+      const fileStats = await fs.stat(srcPath);
+
+      if (fileStats.isDirectory()) {
+        // Recursively copy subdirectories
+        await copyDirectory(srcPath, destPath);
+      } else {
+        // Copy file
+        await fs.copyFile(srcPath, destPath);
+      }
+    }
+
+    console.log(`Directory ${src} copied to ${dest}`);
+  } catch (error) {
+    console.error("Error copying directory:", error);
+  }
+}
+
+
+async function downloadFile(url, outputPath) {
+  const dir = dirname(outputPath);
+  await mkdirSync(dir, { recursive: true });
+
+  const file = createWriteStream(outputPath);
+
+  return new Promise((resolve, reject) => {
+    https.get(url, (response) => {
+      if (response.statusCode !== 200) {
+        reject(new Error(`Failed to download '${url}' (status code: ${response.statusCode})`));
+        return;
+      }
+
+      response.pipe(file);
+      file.on('finish', () => {
+        file.close();
+        resolve();
+      });
+    }).on('error', (err) => {
+      file.destroy();
+      reject(err);
+    });
+  });
 }
 
 
@@ -49,12 +77,12 @@ let go_build_command = "";
 
 // set the go_build_command depending on the current operating system
 if (os.platform().indexOf("win") != -1) {
-    // building on windows
-    go_build_command = `set GOOS=${target_operating_system}&& set GOARCH=amd64&& go build -C ..`;
+  // building on windows
+  go_build_command = `set GOOS=${target_operating_system}&& set GOARCH=amd64&& go build -C ..`;
 
 } else if (os.platform().indexOf("linux") != -1) {
-    // building on linux
-    go_build_command = `GOOS=${target_operating_system} GOARCH=amd64 go build -C ..`;
+  // building on linux
+  go_build_command = `GOOS=${target_operating_system} GOARCH=amd64 go build -C ..`;
 
 } else throw new Error(`Building on unsupported operating system: ${os.platform()}`);
 
@@ -73,11 +101,11 @@ console.log(output);
 
 // create an empty dist directory
 if (!existsSync("../dist")) {
-    mkdirSync("../dist", { recursive: true });
+  mkdirSync("../dist", { recursive: true });
 } else {
-    console.log("Dist folder already exists. Deleting folder...");
-    rmSync("../dist", { recursive: true, force: true });
-    mkdirSync("../dist", { recursive: true });
+  console.log("Dist folder already exists. Deleting folder...");
+  rmSync("../dist", { recursive: true, force: true });
+  mkdirSync("../dist", { recursive: true });
 }
 console.log("Dist folder created successfully.");
 
@@ -87,4 +115,16 @@ console.log("Copied fileserver executable into dist successfully.")
 
 copyDirectory("./dist", "../dist")
 
-copyDirectory("../models", "../dist/models")
+// download models
+let model_file_urls = [
+  ["https://huggingface.co/Xenova/bart-large-cnn/resolve/main/config.json", "summarization/Xenova/bart-large-cnn/config.json"],
+  ["https://huggingface.co/Xenova/bart-large-cnn/resolve/main/generation_config.json", "summarization/Xenova/bart-large-cnn/generation_config.json"],
+  ["https://huggingface.co/Xenova/bart-large-cnn/resolve/main/tokenizer.json", "summarization/Xenova/bart-large-cnn/tokenizer.json"],
+  ["https://huggingface.co/Xenova/bart-large-cnn/resolve/main/tokenizer_config.json", "summarization/Xenova/bart-large-cnn/tokenizer_config.json"],
+  ["https://huggingface.co/Xenova/bart-large-cnn/resolve/main/onnx/decoder_model_merged_quantized.onnx","summarization/Xenova/bart-large-cnn/decoder_model_merged_quantized.onnx"],
+  ["https://huggingface.co/Xenova/bart-large-cnn/resolve/main/onnx/encoder_model_quantized.onnx", "summarization/Xenova/bart-large-cnn/encoder_model_quantized.onnx"]
+];
+for (let url of model_file_urls) {
+  console.log(url);
+  downloadFile(url[0], `../dist/models/${url[1]}`);
+}
