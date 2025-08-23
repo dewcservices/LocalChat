2602717dcec3147487dc4664bb94940633bd5b58
TheConnorBell <cabell2003@gmail.com>
2025-08-23 22:00:09 +0930

    Add error handling

--- web_app/src/components/ModelTesting.jsx
+++ web_app/src/components/ModelTesting.jsx
@@ -6,6 +6,7 @@ import modelTestingStyles from './ModelTesting.module.css';
 function ModelTesting() {
   const [selectedModels, setSelectedModels] = createSignal([]);
 
+  const allowedModelTypes = ["summarization","question-answering","translation"];
   const defaultLanguages = {"Acehnese (Arabic script)":"ace_Arab","Acehnese (Latin script)":"ace_Latn","Mesopotamian Arabic":"acm_Arab","Taâ€™izzi-Adeni Arabic":"acq_Arab","Tunisian Arabic":"aeb_Arab","Afrikaans":"afr_Latn","South Levantine Arabic":"ajp_Arab","Akan":"aka_Latn","Amharic":"amh_Ethi","North Levantine Arabic":"apc_Arab","Modern Standard Arabic":"arb_Arab","Modern Standard Arabic (Romanized)":"arb_Latn","Najdi Arabic":"ars_Arab","Moroccan Arabic":"ary_Arab","Egyptian Arabic":"arz_Arab","Assamese":"asm_Beng","Asturian":"ast_Latn","Awadhi":"awa_Deva","Central Aymara":"ayr_Latn","South Azerbaijani":"azb_Arab","North Azerbaijani":"azj_Latn","Bashkir":"bak_Cyrl","Bambara":"bam_Latn","Balinese":"ban_Latn","Belarusian":"bel_Cyrl","Bemba":"bem_Latn","Bengali":"ben_Beng","Bhojpuri":"bho_Deva","Banjar (Arabic script)":"bjn_Arab","Banjar (Latin script)":"bjn_Latn","Standard Tibetan":"bod_Tibt","Bosnian":"bos_Latn","Buginese":"bug_Latn","Bulgarian":"bul_Cyrl","Catalan":"cat_Latn","Cebuano":"ceb_Latn","Czech":"ces_Latn","Chokwe":"cjk_Latn","Central Kurdish":"ckb_Arab","Crimean Tatar":"crh_Latn","Welsh":"cym_Latn","Danish":"dan_Latn","German":"deu_Latn","Southwestern Dinka":"dik_Latn","Dyula":"dyu_Latn","Dzongkha":"dzo_Tibt","Greek":"ell_Grek","English":"eng_Latn","Esperanto":"epo_Latn","Estonian":"est_Latn","Basque":"eus_Latn","Ewe":"ewe_Latn","Faroese":"fao_Latn","Fijian":"fij_Latn","Finnish":"fin_Latn","Fon":"fon_Latn","French":"fra_Latn","Friulian":"fur_Latn","Nigerian Fulfulde":"fuv_Latn","Scottish Gaelic":"gla_Latn","Irish":"gle_Latn","Galician":"glg_Latn","Guarani":"grn_Latn","Gujarati":"guj_Gujr","Haitian Creole":"hat_Latn","Hausa":"hau_Latn","Hebrew":"heb_Hebr","Hindi":"hin_Deva","Chhattisgarhi":"hne_Deva","Croatian":"hrv_Latn","Hungarian":"hun_Latn","Armenian":"hye_Armn","Igbo":"ibo_Latn","Ilocano":"ilo_Latn","Indonesian":"ind_Latn","Icelandic":"isl_Latn","Italian":"ita_Latn","Javanese":"jav_Latn","Japanese":"jpn_Jpan","Kabyle":"kab_Latn","Jingpho":"kac_Latn","Kamba":"kam_Latn","Kannada":"kan_Knda","Kashmiri (Arabic script)":"kas_Arab","Kashmiri (Devanagari script)":"kas_Deva","Georgian":"kat_Geor","Central Kanuri (Arabic script)":"knc_Arab","Central Kanuri (Latin script)":"knc_Latn","Kazakh":"kaz_Cyrl","KabiyÃ¨":"kbp_Latn","Kabuverdianu":"kea_Latn","Khmer":"khm_Khmr","Kikuyu":"kik_Latn","Kinyarwanda":"kin_Latn","Kyrgyz":"kir_Cyrl","Kimbundu":"kmb_Latn","Northern Kurdish":"kmr_Latn","Kikongo":"kon_Latn","Korean":"kor_Hang","Lao":"lao_Laoo","Ligurian":"lij_Latn","Limburgish":"lim_Latn","Lingala":"lin_Latn","Lithuanian":"lit_Latn","Lombard":"lmo_Latn","Latgalian":"ltg_Latn","Luxembourgish":"ltz_Latn","Luba-Kasai":"lua_Latn","Ganda":"lug_Latn","Luo":"luo_Latn","Mizo":"lus_Latn","Standard Latvian":"lvs_Latn","Magahi":"mag_Deva","Maithili":"mai_Deva","Malayalam":"mal_Mlym","Marathi":"mar_Deva","Minangkabau (Arabic script)":"min_Arab","Minangkabau (Latin script)":"min_Latn","Macedonian":"mkd_Cyrl","Plateau Malagasy":"plt_Latn","Maltese":"mlt_Latn","Meitei (Bengali script)":"mni_Beng","Halh Mongolian":"khk_Cyrl","Mossi":"mos_Latn","Maori":"mri_Latn","Burmese":"mya_Mymr","Dutch":"nld_Latn","Norwegian Nynorsk":"nno_Latn","Norwegian BokmÃ¥l":"nob_Latn","Nepali":"npi_Deva","Northern Sotho":"nso_Latn","Nuer":"nus_Latn","Nyanja":"nya_Latn","Occitan":"oci_Latn","West Central Oromo":"gaz_Latn","Odia":"ory_Orya","Pangasinan":"pag_Latn","Eastern Panjabi":"pan_Guru","Papiamento":"pap_Latn","Western Persian":"pes_Arab","Polish":"pol_Latn","Portuguese":"por_Latn","Dari":"prs_Arab","Southern Pashto":"pbt_Arab","Ayacucho Quechua":"quy_Latn","Romanian":"ron_Latn","Rundi":"run_Latn","Russian":"rus_Cyrl","Sango":"sag_Latn","Sanskrit":"san_Deva","Santali":"sat_Olck","Sicilian":"scn_Latn","Shan":"shn_Mymr","Sinhala":"sin_Sinh","Slovak":"slk_Latn","Slovenian":"slv_Latn","Samoan":"smo_Latn","Shona":"sna_Latn","Sindhi":"snd_Arab","Somali":"som_Latn","Southern Sotho":"sot_Latn","Spanish":"spa_Latn","Tosk Albanian":"als_Latn","Sardinian":"srd_Latn","Serbian":"srp_Cyrl","Swati":"ssw_Latn","Sundanese":"sun_Latn","Swedish":"swe_Latn","Swahili":"swh_Latn","Silesian":"szl_Latn","Tamil":"tam_Taml","Tatar":"tat_Cyrl","Telugu":"tel_Telu","Tajik":"tgk_Cyrl","Tagalog":"tgl_Latn","Thai":"tha_Thai","Tigrinya":"tir_Ethi","Tamasheq (Latin script)":"taq_Latn","Tamasheq (Tifinagh script)":"taq_Tfng","Tok Pisin":"tpi_Latn","Tswana":"tsn_Latn","Tsonga":"tso_Latn","Turkmen":"tuk_Latn","Tumbuka":"tum_Latn","Turkish":"tur_Latn","Twi":"twi_Latn","Central Atlas Tamazight":"tzm_Tfng","Uyghur":"uig_Arab","Ukrainian":"ukr_Cyrl","Umbundu":"umb_Latn","Urdu":"urd_Arab","Northern Uzbek":"uzn_Latn","Venetian":"vec_Latn","Vietnamese":"vie_Latn","Waray":"war_Latn","Wolof":"wol_Latn","Xhosa":"xho_Latn","Eastern Yiddish":"ydd_Hebr","Yoruba":"yor_Latn","Yue Chinese":"yue_Hant","Chinese (Simplified)":"zho_Hans","Chinese (Traditional)":"zho_Hant","Standard Malay":"zsm_Latn","Zulu":"zul_Latn"};
 
   const [menuIsOpen, setMenuIsOpen] = createSignal([]);
@@ -35,6 +36,12 @@ function ModelTesting() {
     let fileText = await configFile.text();
     fileText = JSON.parse(fileText);
 
+    // Check if model type is supported for benchmarking
+    if (!allowedModelTypes.find((element) => element == fileText.task)) {
+      alert("Model type '" + fileText.task + "' not supported for benchmarking");
+      return;
+    }
+
     // Create model JSON to store model.
     const model = {
       name: fileText.modelName,
@@ -43,10 +50,10 @@ function ModelTesting() {
       languages: fileText.languages,
     };
 
-    console.log(fileText);
-
     setSelectedModels([...selectedModels(), model]);
-    console.log(selectedModels())
+
+    // Reset the input incase the model is removed and needs to be re-added.
+    event.target.value = "";
   }
 
   const benchmarkModels = async () => {
@@ -77,6 +84,7 @@ function ModelTesting() {
     let totalTime;
 
     document.getElementById("modelInput").disabled = true;
+    document.getElementById("modelInputLabel").setAttribute("disabled","disabled");
     document.getElementById("benchmarkButton").disabled = true;
     document.getElementById("clearButton").disabled = true;
 
@@ -84,6 +92,9 @@ function ModelTesting() {
     for (let i = 0; i < modelList.length; i++) {
       const model = modelList[i];
       const currentRow = table.rows[i+1];
+
+      table.rows[i].classList.remove(modelTestingStyles.currentTableRow);
+      currentRow.classList.add(modelTestingStyles.currentTableRow);
 
       let generator;
       currentRow.cells[tableUploadTimeCol].innerText = "Uploading: 0/" + globalModelRunCount;
@@ -126,7 +137,15 @@ function ModelTesting() {
           });
         };
     
-        generator = await pipeline(model.modelType, model.name);
+        // Create pipeline for the generator;
+        try {
+          generator = await pipeline(model.modelType, model.name);
+        } catch (error) {
+          generator = null;
+          console.log(error);
+          continue;
+        }
+        
 
         endTime = performance.now();
 
@@ -150,6 +169,11 @@ function ModelTesting() {
       }
 
       let totalUploadTimes = benchmarkData()[model.name + "-Upload"];
+
+      if (generator == null) {
+        currentRow.cells[tableUploadTimeCol].innerText = "pipeline creation failed";
+        continue;
+      }
 
       // Get the average upload time by using the reduce pattern to sum and then divide by total amount.
       let avgUploadTime = totalUploadTimes.reduce((a, b) => a + b) / totalUploadTimes.length;
@@ -184,7 +208,16 @@ function ModelTesting() {
 
           // Benchmark the model.
           startTime = performance.now();
-          output = await generator(userInput, { max_new_tokens: 100});
+
+          // Generate model output
+          try {
+            output = await generator(userInput, { max_new_tokens: 100});
+          } catch (error) {
+            console.log(error);
+            output == null;
+            continue;
+          }
+          
           endTime = performance.now();
           output = output[0].summary_text;
 
@@ -206,7 +239,16 @@ function ModelTesting() {
 
           // Benchmark the model.
           startTime = performance.now();
-          output = await generator(context, question);
+
+          // Generate model output
+          try {
+            output = await generator(context, question);
+          } catch (error) {
+            console.log(error);
+            output == null;
+            continue;
+          }
+          
           endTime = performance.now();
           output = output.answer;
 
@@ -232,7 +274,16 @@ function ModelTesting() {
 
           // Benchmark the model
           startTime = performance.now();
-          output = await generator(userInput, {src_lang: fromLanguageCode, tgt_lang: toLanguageCode});
+
+          // Generate model output
+          try {
+            output = await generator(userInput, {src_lang: fromLanguageCode, tgt_lang: toLanguageCode});
+          } catch (error) {
+            console.log(error);
+            output == null;
+            continue;
+          }
+          
           endTime = performance.now();
           output = output[0].translation_text;
         }
@@ -265,10 +316,17 @@ function ModelTesting() {
       currentRow.cells[tableGenerationTimeCol].title = totalGenerationTimes;
       currentRow.cells[tableMessageCol].innerText = output;
 
+      currentRow.classList.remove(modelTestingStyles.currentTableRow);
+
+      // Remove the pipeline to prevent any errors when switching to the next model.
+      generator.dispose();
+
       await new Promise(requestAnimationFrame);
     }
 
+    // Undisable the buttons
     document.getElementById("modelInput").disabled = false;
+    document.getElementById("modelInputLabel").removeAttribute("disabled");
     document.getElementById("benchmarkButton").disabled = false;
     document.getElementById("clearButton").disabled = false;
 
@@ -325,13 +383,13 @@ function ModelTesting() {
 
         <div>
           {/* Select Model/s for benchmarking */}
+          <input type="file" id="modelInput" className='hidden' webkitdirectory multiple onChange={addModel} />
           <label for="modelInput" id="modelInputLabel" class={modelTestingStyles.inputButton} className='inputButton'>
             Select Models
           </label>
-          <input type="file" id="modelInput" className='hidden' webkitdirectory multiple onChange={addModel} />
                       
-          <button id="benchmarkButton" class={modelTestingStyles.inputButton} onClick={benchmarkModels}>Benchmark</button>
-          <button id="clearButton" class={modelTestingStyles.inputButton} onClick={clearModels}>Clear Models</button>
+          <button id="benchmarkButton" class={modelTestingStyles.inputButton} onClick={benchmarkModels} disabled={selectedModels().length < 1}>Benchmark</button>
+          <button id="clearButton" class={modelTestingStyles.inputButton} onClick={clearModels } disabled={selectedModels().length < 1}>Clear Models</button>
         </div>
 
         <div>
